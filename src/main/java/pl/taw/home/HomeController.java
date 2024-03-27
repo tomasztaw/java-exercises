c77fc094c67ed4ec1b72bfade52ed281c1a1e843
tomasztaw <tomasz.wegrzyn.wro@gmail.com>
2024-03-27 22:20:04 +0100

    initial commit

---
+++ src/main/java/pl/taw/home/HomeController.java
@@ -0,0 +1,30 @@
+/**
+ * Created by tomasz_taw
+ * Date: 27.03.2024
+ * Time: 21:25
+ * Project Name: java-exercises
+ * Description:
+ */
+package pl.taw.home;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public String home() {
+        return "index";
+    }
+
+    @GetMapping("/rental-app")
+    public String rentalApp() {
+        return "rental-app";
+    }
+
+    @GetMapping("/lottery")
+    public String lottery() {
+        return "lottery";
+    }
+}
